1269c59e049d283ad740c2edfc0ac4610200d4bd
Silvio Heuberger <silvio.heuberger@gmail.com>
2025-02-04 22:33:21 +0100

    First skeleton implementation of lox interpreter in rust

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::io::{self, BufRead};
+use std::{env, process::exit};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: rlox [script]");
+        exit(64);
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(filename: &str) {
+    let contents =
+        fs::read_to_string(filename).expect("Should have been able to read the script file");
+    run(&contents);
+}
+
+fn run_prompt() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        run(&line.expect("Unable to read line from stdin!"));
+    }
+}
+
+fn run(script: &str) {
+    println!("Running {}", script);
+}
